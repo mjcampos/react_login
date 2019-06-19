f1428d9d4294b5fdf437cbe59119c5bfcc8eef37
Michael Campos <camposmj89@gmail.com>
2019-06-18 20:45:52 -0700

    Added a basic validator for username and password, which includes the creation of a Error Modal

---
+++ src/components/ErrorModal.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+const ErrorModal = ({errorMessage}) => {
+	return (
+		<div>{errorMessage}</div>
+	);
+}
+
+export default ErrorModal;
--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import { Component } from 'react';
+import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import ErrorModal from './ErrorModal';
 
 import { userActions } from '../actions';
 
-export class LoginPage extends Component {
+class LoginPage extends Component {
     constructor(props) {
         super(props);
 
@@ -14,44 +14,62 @@ export class LoginPage extends Component {
         this.state = {
             username: '',
             password: '',
-            submitted: false
+            submitted: false,
+            error: false,
+            errorMessage: ''
         };
 
-        this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    handleChange(e) {
-    }
-
     handleSubmit(e) {
-        
+        e.preventDefault();
+
+        var {username, password} = this.state;
+
+        // Set the submitted state to true
+        this.setState({submitted: true});
+
+        // Need to validate
+        if (username && password) {
+            console.log("Logging in");
+            this.setState({error: false});
+        } else {
+            this.setState({
+                error: true,
+                errorMessage: 'Invalide Username or Password'
+            });
+        }
     }
 
     render() {
-        const { username, password, submitted } = this.state;
-        
+        const { username, password, submitted, error, errorMessage } = this.state;
+
         return (
             <div className="col-md-6 col-md-offset-3">
                 <h2>Login</h2>
 
-                <form name="form">
+                {error && <ErrorModal errorMessage={errorMessage}/>}
+
+                <form name="form" onSubmit={this.handleSubmit}>
                     <div className={'form-group' + (submitted && !username ? ' has-error' : '')}>
                         <label htmlFor="username">Username</label>
-                        <input type="text" className="form-control username" name="username" />
+                        <input type="text" className="form-control username" name="username" onChange={e => this.setState({username: e.target.value})} />
                         {submitted && !username &&
                             <div className="help-block">Username is required</div>
                         }
                     </div>
+
                     <div className={'form-group' + (submitted && !password ? ' has-error' : '')}>
                         <label htmlFor="password">Password</label>
-                        <input type="password" className="form-control" name="password"/>
+                        <input type="password" className="form-control" name="password" onChange={e => this.setState({password: e.target.value})}/>
                         {submitted && !password &&
                             <div className="help-block">Password is required</div>
                         }
                     </div>
+
                     <div className="form-group">
-                        <button className="btn btn-primary">Login</button>
+                        <button className="btn btn-primary" >Login</button>
                     </div>
                 </form>
             </div>
@@ -60,7 +78,9 @@ export class LoginPage extends Component {
 }
 
 function mapStateToProps(state) {
-
+    return state;
 }
 
-export { LoginPage as TestLoginPage };
+// export { LoginPage as TestLoginPage };
+
+export default connect(mapStateToProps, null)(LoginPage);
